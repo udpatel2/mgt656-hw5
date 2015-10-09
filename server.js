f1ac7e0c11a483ad634e47d784f2ff522f75e937
udpatel2 <udpatel2@gmail.com>
2015-10-09 03:41:37 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I saw a ninja today');
+});
+
+app.get('/netid', function(request, response){
+    response.send('Put netid here');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('helpful-salmon');
 });
 
 app.listen(process.env.PORT || 4000);
